44829cea02d8486832968b3ff69f3c2bbbde8dd5
nvkantonio <nvkantonio@gmail.com>
2025-09-22 17:00:20 +0200

    refactor: exeptions

--- lib/src/http_wrapper_exeptions.dart
+++ lib/src/http_wrapper_exeptions.dart
@@ -1,18 +1,11 @@
 import 'package:http/http.dart';
 
 // Api response exeption
-abstract class ResponseExeption implements Exception {
-  const ResponseExeption({
-    this.message = '',
-    this.source,
-    this.causedError,
-    this.response,
-  });
-
-  final String message;
-  final dynamic source;
-  final Object? causedError;
-  final BaseResponse? response;
+abstract interface class ResponseExeption implements Exception {
+  String get message;
+  dynamic get source;
+  Object? get causedError;
+  BaseResponse? get response;
 
   @override
   String toString() {
@@ -20,14 +13,42 @@ abstract class ResponseExeption implements Exception {
   }
 }
 
-/// Use on invalid api response exeptions
-class InvalidResponseExeption extends ResponseExeption {
-  const InvalidResponseExeption({
-    super.message,
-    super.source,
-    super.response,
-    super.causedError,
+/// Use on valid api response exeptions
+class HandeledResponseExeption implements ResponseExeption {
+  const HandeledResponseExeption({
+    this.message = '',
+    this.source,
+    this.causedError,
+    this.response,
   });
+
+  @override
+  final String message;
+  @override
+  final dynamic source;
+  @override
+  final Object? causedError;
+  @override
+  final Response? response;
+}
+
+/// Use on invalid api response exeptions
+class InvalidResponseExeption implements ResponseExeption {
+  const InvalidResponseExeption({
+    this.message = '',
+    this.source,
+    this.causedError,
+    this.response,
+  });
+
+  @override
+  final String message;
+  @override
+  final dynamic source;
+  @override
+  final Object? causedError;
+  @override
+  final Response? response;
 }
 
 /// Use on parse exeptions
@@ -40,22 +61,12 @@ class ResponseParseExeption extends InvalidResponseExeption {
   });
 }
 
-/// Use on valid api response exeptions
-abstract class HandeledResponseExeption extends ResponseExeption {
-  const HandeledResponseExeption({
-    super.message,
-    super.source,
-    super.response,
-    super.causedError,
-  });
-}
-
 /// Use on failed validation api response
 class ResponseValidationExeption extends HandeledResponseExeption {
   const ResponseValidationExeption({
-    super.message,
+    super.message = '',
     super.source,
-    super.response,
     super.causedError,
+    super.response,
   });
 }
